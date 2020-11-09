9e0219943a2fddc6b1d3c4b3c8f1a2b8fd778f9b
hajri-nizar <nizar.hajri@sesame.com.tn>
2020-11-09 16:59:30 +0100

    step 6: create class DaoBook

--- src/dao/DaoBook.java
+++ src/dao/DaoBook.java
@@ -1,5 +1,42 @@
 package dao;
 
+import entities.Book;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DaoBook {
 
+    private Statement stmt;
+
+    public DaoBook(Statement stmt) {
+        this.stmt = stmt;
+    }
+
+    public void addBook (Book book) throws SQLException {
+        String query = "INSERT INTO book (id, title, price, author, releaseDate) VALUES (" + book.getId() + ", '" + book.getTitle() + "', '" + book.getPrice() + "', '" + book.getAuthor() + "', '" + book.getDate() + "')";
+        stmt.executeUpdate(query);
+    }
+
+    public List<Book> listBook() throws SQLException {
+        ResultSet rs= stmt.executeQuery("select * from book");
+        List<Book> list = new ArrayList<>();
+
+        while (rs.next()) {
+            Book book = new Book();
+
+            book.setId(rs.getInt("id"));
+            book.setAuthor(rs.getString("author"));
+            book.setDate(rs.getDate("releaseDate"));
+            book.setPrice(rs.getDouble("price"));
+            book.setTitle(rs.getString("title"));
+
+            list.add(book);
+        }
+
+        return list;
+    }
 }
--- src/entities/Book.java
+++ src/entities/Book.java
@@ -3,44 +3,46 @@ package entities;
 import java.sql.Date;
 
 public class Book {
-private int id ; 
-private String title ;
-private double price ; 
-private String author ; 
-private Date date ;
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public String getTitle() {
-	return title;
-}
-public void setTitle(String title) {
-	this.title = title;
-}
-public double getPrice() {
-	return price;
-}
-public void setPrice(double price) {
-	this.price = price;
-}
-public String getAuthor() {
-	return author;
-}
-public void setAuthor(String author) {
-	this.author = author;
-}
-public Date getDate() {
-	return date;
-}
-public void setDate(Date date) {
-	this.date = date;
-}
-@Override
-public String toString() {
-	return "Book [id=" + id + ", title=" + title + ", price=" + price + ", author=" + author + ", date=" + date + "]";
-} 
+	private int id;
+	private String title;
+	private double price;
+	private String author;
+	private Date date;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", price=" + price + ", author=" + author + ", date=" + date + "]";
+	}
 
 }
