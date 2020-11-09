e60da4ef7a8a97339693519b4588b4b9a9ad1ed9
Belhedi94 <rafaa.belhedi@gmail.com>
2020-11-09 18:54:29 +0100

    strp2 : Implementation de la fonction main

--- src/dao/DaoBook.java
+++ src/dao/DaoBook.java
@@ -10,33 +10,35 @@ import java.util.List;
 
 public class DaoBook {
 
-    private Statement stmt;
+	private Statement stmt;
 
-    public DaoBook(Statement stmt) {
-        this.stmt = stmt;
-    }
+	public DaoBook(Statement stmt) {
+		this.stmt = stmt;
+	}
 
-    public void addBook (Book book) throws SQLException {
-        String query = "INSERT INTO book (id, title, price, author, releaseDate) VALUES (" + book.getId() + ", '" + book.getTitle() + "', '" + book.getPrice() + "', '" + book.getAuthor() + "', '" + book.getDate() + "')";
-        stmt.executeUpdate(query);
-    }
+	public void addBook(Book book) throws SQLException {
+		String query = "INSERT INTO book (id, title, price, author, releaseDate) VALUES (" + book.getId() + ", '"
+				+ book.getTitle() + "', '" + book.getPrice() + "', '" + book.getAuthor() + "', '" + book.getDate()
+				+ "')";
+		stmt.executeUpdate(query);
+	}
 
-    public List<Book> listBook() throws SQLException {
-        ResultSet rs= stmt.executeQuery("select * from book");
-        List<Book> list = new ArrayList<>();
+	public List<Book> listBook() throws SQLException {
+		ResultSet rs = stmt.executeQuery("select * from book");
+		List<Book> list = new ArrayList<>();
 
-        while (rs.next()) {
-            Book book = new Book();
+		while (rs.next()) {
+			Book book = new Book();
 
-            book.setId(rs.getInt("id"));
-            book.setAuthor(rs.getString("author"));
-            book.setDate(rs.getDate("releaseDate"));
-            book.setPrice(rs.getDouble("price"));
-            book.setTitle(rs.getString("title"));
+			book.setId(rs.getInt("id"));
+			book.setAuthor(rs.getString("author"));
+			book.setDate(rs.getDate("releaseDate"));
+			book.setPrice(rs.getDouble("price"));
+			book.setTitle(rs.getString("title"));
 
-            list.add(book);
-        }
+			list.add(book);
+		}
 
-        return list;
-    }
+		return list;
+	}
 }
